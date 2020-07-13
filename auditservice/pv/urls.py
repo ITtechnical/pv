ea07874a6ebe49d3fd29b469e9d2b68ab01fa44f
ITtechnical <68113347+ITtechnical@users.noreply.github.com>
2020-07-13 11:55:33 +0000

    password reset
    Co-Authored-By: terkpeh <terkpeh@users.noreply.github.com>

--- auditservice/pv/urls.py
+++ auditservice/pv/urls.py
@@ -46,14 +46,7 @@ urlpatterns = [
         path('pv/accountableimpress/csv',views.accountableimpresss, name= 'accountableimpresscsv'),
         path('pv/nonaccountableimpress/csv',views.nonaccountableimpresss, name= 'nonaccountableimpresscsv'),
         path('pv/beneficiaries/csv',views.beneficiariess, name= 'beneficiariescsv'),
-        # path('pv/report/',views. ReportView.as_view(), name ='report'),
-        # path('reset_password/',auth_views.PasswordResetView.as_view(),name="reset_password"),
-
-        # path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
-
-        # path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
-
-        # path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
+      
       ]
  
 
