94f7ffada8eb716e05f29a8f608a40f50ec6dbb7
ITtechnical <68113347+ITtechnical@users.noreply.github.com>
2021-02-26 15:14:04 +0000

    Update settings.py

--- auditservice/auditservice/settings.py
+++ auditservice/auditservice/settings.py
@@ -136,12 +136,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_URL = 'log'
 
 
-#SMTP Configuration
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST ='smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='audit.ittechnical@gmail.com'
-EMAIL_HOST_PASSWORD = 'lovia2020'
 
